c8f3bae51b6800dad85db69864197a6ea54488be
Navarrete <aaldiitoo@gmail.com>
2022-05-13 16:47:39 -0500

    Creacion tabla huesped y tabla reserva

--- DDLyDML.sql
+++ DDLyDML.sql
@@ -8,7 +8,7 @@ Equipo:
 */
 
 -- CREACION DE TABLAS
--- Tabla Personal
+-- Tabla Personal Diego
 CREATE TABLE personal(
     clavePersonal CHAR(4),
     nomPer VARCHAR2(30) NOT NULL,
@@ -26,7 +26,32 @@ CREATE TABLE personal(
     REFERENCES alojamiento (clvAlo) ON DELETE CASCADE
 );
 
---Tabla Huesped
+--Tabla Huesped Aldo
 CREATE TABLE Huesped(
-    
+    claveHuesped CHAR(4),
+    nomHues VARCHAR2(30) NOT NULL,
+    apPatHuesped VARCHAR2(30) NOT NULL,
+    apMatHuesped VARCHAR2(30),
+    calleHues VARCHAR2(30) NOT NULL,
+    CPHues NUMBER(5) NOT NULL,
+    paisHues VARCHAR2(20) NOT NULL,
+    telHues NUMBER(10) NOT NULL,
+    CONSTRAINT PK_Huesped PRIMARY KEY (claveHuesped),
+)
+--Tabla Alojamiento
+
+--Tabla Habitación
+
+
+--Tabla Reserva Aldo
+CREATE TABLE Reserva(
+    noHab CHAR(4),
+    clvAlo CHAR(4),
+    claveHuesped CHAR(4),
+    inicioReserva DATE,
+    finReserva DATE,
+    CONSTRAINT PK_Reserva PRIMARY KEY (noHab, clvAlo, claveHuesped),
+    CONSTRAINT FK_Reserva_Alojamiento FOREIGN KEY (clvAlo) REFERENCES alojamiento,
+    CONSTRAINT FK_Reserva_Huesped FOREIGN KEY (claveHuesped) REFERENCES Huesped ON DELETE SET NULL,
+    CONSTRAINT FK_Reserva_Habitacion FOREIGN KEY (noHab) REFERENCES Habitacion
 )

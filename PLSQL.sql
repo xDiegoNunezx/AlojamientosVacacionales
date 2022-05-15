54661bfdddcdfea6ba541e2175a322f2a6fbca64
CiberSword <CiberSword@hotmail.com>
2022-05-15 12:49:25 -0500

    Co-authored-by: Diego Ignacio Núñez Hernández  <xDiegoNunezx@users.noreply.github.com> Co-authored-by: Aldo Navarrete <aldonavarretefp@users.noreply.github.com>
    Avance Inserciones

--- PLSQL.sql
+++ PLSQL.sql
@@ -111,35 +111,40 @@ END spVacaciones;
     huéspedes y los alojamientos.
 */
 
+
+
 /*
     En el momento en que una reservación es cancelada el estatus de la 
     habitación debe de regresar a su estado original.
 */
 CREATE OR REPLACE VIEW vwReserva
 AS
-SELECT noHab, nomHues, inicioReserva, finReserva, estatusHab FROM RESERVA
-JOIN HUESPED using (claveHuesped)
-JOIN Habitacion using (noHab)
+SELECT noHab, clvAlo, nomHues, inicioReserva, finReserva, estatusHab FROM RESERVA
+NATURAL JOIN Huesped
+NATURAL JOIN HABITACION;
 
 
 CREATE OR REPLACE TRIGGER tgReservacionCancelada
 INSTEAD OF DELETE ON vwReserva --Opera en la vista
 FOR EACH ROW
 BEGIN
-	UPDATE RESERVA SET status = 'DISPONIBLE'
-	WHERE noHab = :OLD.numFact
+	UPDATE HABITACION SET estatusHab = 'DISPONIBLE'
+	WHERE noHab = :OLD.noHab
     AND clvAlo = :OLD.clvAlo;
 END tgReservacionCancelada;
 /
 
--- Dar de alta cliente
+-- ************** PROCEDIMIENTOS ***************
 
-CREATE OR REPLACE PROCEDURE spAltaPersonal(
-   
-)
 
--- Dar alta alojamiento
 
+/*  
+    PROCEDIMIENTO para crear alojamientos
+*/
+
+CREATE SEQUENCE ID_ALO_SEQ
+START WITH 1
+INCREMENT BY 1;
 
 CREATE OR REPLACE PROCEDURE spAltaAlojamiento(
    nomAlo IN VARCHAR,
@@ -151,6 +156,86 @@ CREATE OR REPLACE PROCEDURE spAltaAlojamiento(
 )
 AS
 BEGIN
-   INSERT INTO Alojamiento(clvAlo,nomAlo,calleAlo,CPAlo,paisAlo,cantidadHab,telAlo) VALUES (ID_ALO_SEQ.NEXT_VAL,nomAlo,calleAlo,CPAlo,paisAlo,cantidadHab,telAlo);
+   INSERT INTO Alojamiento(clvAlo,nomAlo,calleAlo,CPAlo,paisAlo,cantidadHab,telAlo) VALUES (ID_ALO_SEQ.NEXTVAL,nomAlo,calleAlo,CPAlo,paisAlo,cantidadHab,telAlo);
 END spAltaAlojamiento;
+/
+
+EXEC spAltaAlojamiento('INN SUR','Calzada del Hueso',85453,'Mexico',50,5513451239);
+EXEC spAltaAlojamiento('INN NORTE','Av. Acueducto',11452,'Mexico',40,5582561158);
+EXEC spAltaAlojamiento('INN ORIENTE','Mario de la Cueva',22356,'Mexico',70,5574951724);
+
+
+/* 
+    PROCEDIMIENTO para dar de alta un personal
+*/
+
+CREATE SEQUENCE ID_PER_SEQ
+START WITH 1
+INCREMENT BY 1;
+
+CREATE OR REPLACE PROCEDURE spAltaPersonal(
+   nomPer IN VARCHAR,
+   apPatPer IN VARCHAR,
+   apMatPer IN VARCHAR,
+   callePer IN VARCHAR,
+   CPPer IN NUMBER,
+   paisPer IN VARCHAR,
+   fechaIngreso IN DATE,
+   RFCPer IN CHAR
+)
+AS
+BEGIN
+   INSERT INTO Personal(clavePersonal,nomPer,apPatPer,apMatPer,callePer,CPPer,paisPer,fechaIngreso,RFCPer) VALUES (ID_PER_SEQ.NEXTVAL,nomPer,apPatPer,apMatPer,callePer,CPPer,paisPer,fechaIngreso,RFCPer);
+END spAltaPersonal;
+/
+
+EXEC spAltaPersonal('Vicente','Sierra','Cruz','Av.Chapultepec',85453,'Mexico',SYSDATE-6,'SICV840520QN4');
+EXEC spAltaPersonal('Daniel','Jarquin','Lopez','2 de Marzo',56334,'Mexico',SYSDATE-12,'JALD011030SG3');
+EXEC spAltaPersonal('Aldo','Navarrete','Zamora','Genaro Nuñez 18',07120,'Mexico',SYSDATE-8,'NAZA200301HD');
+EXEC spAltaPersonal('Diego','Nuñez','Hernandez','Juarez 19',21312,'Mexico',SYSDATE-9,'NHDI123124HS');
+EXEC spAltaPersonal('Quetzal','Tavera','Martinez','Degollado 215',41236,'Mexico',SYSDATE-4,'TAMQ010605H82');
+EXEC spAltaPersonal('Ana','Fragoso','Islas','Constitucion 17',54879,'Mexico',SYSDATE-34,'FAIA001218EM4');
+EXEC spAltaPersonal('Edyth','Dunsford',NULL,'1416 Hamilton Street',12312,'Canada',SYSDATE-3,'DUE001218Q710');
+EXEC spAltaPersonal('Merle','Sauriol',NULL,'42 Place du Jeu de Paume',54321,'Francia',SYSDATE-4,'SQME13412HDS');
+EXEC spAltaPersonal('Freddie','Law',NULL,'213 Harvest Lane',12341,'Estados Unidos',SYSDATE-1,'LFW23GF234ASD');
+
+/* 
+    PROCEDIMIENTO Para Asignar un Personal a Alojamiento 
+*/
+
+CREATE OR REPLACE PROCEDURE spAsignaPersonalAlo(
+    vclvPersonal IN CHAR,
+    vclvAlo IN CHAR
+)
+AS
+BEGIN
+    UPDATE Alojamiento
+    SET clavePersonal = vclvPersonal
+    WHERE clvAlo = vclvAlo;
+    
+    UPDATE Personal
+    SET clvAlo = vclvAlo
+    WHERE clavePersonal = vclvPersonal;
+END;
+/
+
+
+EXEC spAsignaPersonalAlo('9','6'); --Diego, Sur
+EXEC spAsignaPersonalAlo('10','8');--Quetzal, Oriente
+EXEC spAsignaPersonalAlo('7','7'); --Dani, Norte
+
+CREATE OR REPLACE PROCEDURE spAltaHuesped(
+   nomHues IN VARCHAR,
+   apPatHues IN VARCHAR,
+   apMatHues IN VARCHAR,
+   calleHues IN VARCHAR,
+   CPHues IN NUMBER,
+   paisHues IN VARCHAR,
+   fechaIngreso IN DATE,
+   RFCPer IN CHAR
+)
+AS
+BEGIN
+   INSERT INTO Personal(clavePersonal,nomPer,apPatPer,apMatPer,callePer,CPPer,paisPer,fechaIngreso,RFCPer) VALUES (ID_PER_SEQ.NEXTVAL,nomPer,apPatPer,apMatPer,callePer,CPPer,paisPer,fechaIngreso,RFCPer);
+END spAltaPersonal;
 /

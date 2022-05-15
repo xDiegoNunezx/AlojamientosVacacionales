3c000e4690917812ab5ee8adea9f973741fe32cb
Diego Núñez <diego.nunezh@ingenieria.unam.edu>
2022-05-14 19:13:52 -0500

    PLSQL

---
+++ PLSQL.sql
@@ -0,0 +1,8 @@
+/*
+Proyecto 2. Alojamientos Vacacionales
+Equipo:
+- Aldo Yael NAVARRETE ZAMORA
+- Daniel Eduardo JARQUIN LÓPEZ
+- Diego Ignacio NÚÑEZ HERNÁNDEZ
+- Uriel Quetzal TAVERA MARRTÍNEZ
+*/

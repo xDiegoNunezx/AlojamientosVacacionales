d2c07589c58bc6be7a8594807fcdcabf31dab2e7
CiberSword <CiberSword@hotmail.com>
2022-05-16 05:05:39 -0500

    Ver. Final PL/SQL
    Co-authored-by: Diego Ignacio Núñez Hernández  <xDiegoNunezx@users.noreply.github.com>
    Co-authored-by: Aldo Navarrete <aldonavarretefp@users.noreply.github.com>
    Co-authored-by: Quetzal-crypto <Quetzal-crypto@users.noreply.github.com>

--- DDLyDML.sql
+++ DDLyDML.sql
@@ -43,9 +43,6 @@ ADD CONSTRAINT personal_fk
 FOREIGN KEY (clvAlo) 
 REFERENCES alojamiento;
 
-ALTER TABLE personal
-DROP CONSTRAINT personal_fk;
-
 --Tabla Huesped Aldo
 CREATE TABLE Huesped(
     claveHuesped CHAR(4),
@@ -58,63 +55,6 @@ CREATE TABLE Huesped(
     telHues NUMBER(10) NOT NULL,
     CONSTRAINT PK_Huesped PRIMARY KEY (claveHuesped)
 );
-
--- Inserciones Tabla Personal
-
-
-
--- Inserciones Tabla Huesped
-
-insert into Huesped (claveHuesped, apPatHuesped, apMatHuesped, calleHues, CPHues, paisHues, telHues) values ('x998', 'Loren', 'Pidcock', '8564 Fremont Alley', 77163, 'China', '4202764245');
-insert into Huesped (claveHuesped, apPatHuesped, apMatHuesped, calleHues, CPHues, paisHues, telHues) values ('o352', 'Teddy', 'Sandham', '2 Melvin Circle', 34773, 'Russia', '2181887567');
-insert into Huesped (claveHuesped, apPatHuesped, apMatHuesped, calleHues, CPHues, paisHues, telHues) values ('p737', 'Tybalt', 'MacFaul', '592 Sycamore Street', 17037, 'Russia', '3952420000');
-insert into Huesped (claveHuesped, apPatHuesped, apMatHuesped, calleHues, CPHues, paisHues, telHues) values ('a626', 'Erinna', 'Forsard', '04 Grim Avenue', 30023, 'Argentina', '7455893160');
-insert into Huesped (claveHuesped, apPatHuesped, apMatHuesped, calleHues, CPHues, paisHues, telHues) values ('l101', 'Dode', 'De la croix', '50 Lakewood Road', 5572, 'Philippines', '3604402634');
-insert into Huesped (claveHuesped, apPatHuesped, apMatHuesped, calleHues, CPHues, paisHues, telHues) values ('l564', 'Ellswerth', 'Dacca', '3 Petterle Point', 78518, 'Venezuela', '2318026033');
-insert into Huesped (claveHuesped, apPatHuesped, apMatHuesped, calleHues, CPHues, paisHues, telHues) values ('e070', 'Wylie', 'Roughsedge', '19 Gulseth Plaza', 71664, 'Russia', '1114497999');
-insert into Huesped (claveHuesped, apPatHuesped, apMatHuesped, calleHues, CPHues, paisHues, telHues) values ('q751', 'Elfrieda', 'Gerhardt', '125 Charing Cross Parkway', 24733, 'China', '6725196946');
-insert into Huesped (claveHuesped, apPatHuesped, apMatHuesped, calleHues, CPHues, paisHues, telHues) values ('x055', 'Ollie', 'Reye', '0195 Clyde Gallagher Lane', 11505, 'Greece', '4918076501');
-insert into Huesped (claveHuesped, apPatHuesped, apMatHuesped, calleHues, CPHues, paisHues, telHues) values ('f364', 'Herb', 'Sager', '134 Alpine Road', 15818, 'Philippines', '1115803403');
-insert into Huesped (claveHuesped, apPatHuesped, apMatHuesped, calleHues, CPHues, paisHues, telHues) values ('p240', 'Grannie', 'Poytheras', '5 Helena Court', 12854, 'China', '9838164287');
-insert into Huesped (claveHuesped, apPatHuesped, apMatHuesped, calleHues, CPHues, paisHues, telHues) values ('t446', 'Juana', 'Breacher', '68 Hagan Point', 84959, 'Brazil', '2479321805');
-insert into Huesped (claveHuesped, apPatHuesped, apMatHuesped, calleHues, CPHues, paisHues, telHues) values ('w751', 'Nerta', 'Baldelli', '512 Prentice Avenue', 35667, 'China', '9463385275');
-insert into Huesped (claveHuesped, apPatHuesped, apMatHuesped, calleHues, CPHues, paisHues, telHues) values ('x863', 'Nyssa', 'Luby', '5543 Petterle Court', 70188, 'Mauritius', '9045256011');
-insert into Huesped (claveHuesped, apPatHuesped, apMatHuesped, calleHues, CPHues, paisHues, telHues) values ('o031', 'Dominique', 'Britten', '49 Fuller Parkway', 33783, 'Vietnam', '9104817030');
-insert into Huesped (claveHuesped, apPatHuesped, apMatHuesped, calleHues, CPHues, paisHues, telHues) values ('y274', 'Rosa', 'Hryniewicz', '5347 Marcy Way', 64541, 'Thailand', '4941087554');
-insert into Huesped (claveHuesped, apPatHuesped, apMatHuesped, calleHues, CPHues, paisHues, telHues) values ('o160', 'Lonna', 'Mulder', '1513 Old Shore Junction', 60717, 'North Korea', '4942858653');
-insert into Huesped (claveHuesped, apPatHuesped, apMatHuesped, calleHues, CPHues, paisHues, telHues) values ('k681', 'Juliann', 'Sorrell', '1 Maple Wood Circle', 96047, 'Brazil', '9714154048');
-insert into Huesped (claveHuesped, apPatHuesped, apMatHuesped, calleHues, CPHues, paisHues, telHues) values ('u520', 'Lyndsie', 'Melluish', '00191 Bluejay Place', 36654, 'Sweden', '8952880228');
-insert into Huesped (claveHuesped, apPatHuesped, apMatHuesped, calleHues, CPHues, paisHues, telHues) values ('v478', 'Husain', 'Vandenhoff', '2093 Rutledge Drive', 18036, 'China', '6694413973');
-insert into Huesped (claveHuesped, apPatHuesped, apMatHuesped, calleHues, CPHues, paisHues, telHues) values ('c714', 'Joana', 'Thorneywork', '53171 Oak Parkway', 14262, 'Brazil', '5897961221');
-insert into Huesped (claveHuesped, apPatHuesped, apMatHuesped, calleHues, CPHues, paisHues, telHues) values ('k271', 'Dedie', 'Loude', '21 Hallows Alley', 50846, 'Belarus', '2717567777');
-insert into Huesped (claveHuesped, apPatHuesped, apMatHuesped, calleHues, CPHues, paisHues, telHues) values ('i180', 'Harriette', 'Kebbell', '5 Eastwood Lane', 31307, 'Mexico', '6822500526');
-insert into Huesped (claveHuesped, apPatHuesped, apMatHuesped, calleHues, CPHues, paisHues, telHues) values ('l552', 'Giuseppe', 'McIlwaine', '262 Ramsey Circle', 15332, 'China', '7797459803');
-insert into Huesped (claveHuesped, apPatHuesped, apMatHuesped, calleHues, CPHues, paisHues, telHues) values ('u069', 'Izaak', 'Clitsome', '52852 Crescent Oaks Court', 66948, 'China', '6724004280');
-insert into Huesped (claveHuesped, apPatHuesped, apMatHuesped, calleHues, CPHues, paisHues, telHues) values ('o842', 'Burty', 'Fudger', '49848 Arrowood Way', 53670, 'Czech Republic', '3754807659');
-insert into Huesped (claveHuesped, apPatHuesped, apMatHuesped, calleHues, CPHues, paisHues, telHues) values ('r021', 'Fina', 'Handover', '28050 Holmberg Circle', 55364, 'Equatorial Guinea', '3441515623');
-insert into Huesped (claveHuesped, apPatHuesped, apMatHuesped, calleHues, CPHues, paisHues, telHues) values ('e545', 'Lilia', 'Dani', '48 Starling Trail', 38955, 'Paraguay', '3442187386');
-insert into Huesped (claveHuesped, apPatHuesped, apMatHuesped, calleHues, CPHues, paisHues, telHues) values ('k760', 'Rochette', 'MacCourt', '1 Michigan Street', 11306, 'China', '8502259171');
-insert into Huesped (claveHuesped, apPatHuesped, apMatHuesped, calleHues, CPHues, paisHues, telHues) values ('t414', 'Anna', 'Bayldon', '74 Petterle Hill', 95004, 'Nigeria', '8787167042');
-insert into Huesped (claveHuesped, apPatHuesped, apMatHuesped, calleHues, CPHues, paisHues, telHues) values ('g408', 'Floria', 'Parres', '4 High Crossing Road', 68390, 'Indonesia', '6691458428');
-insert into Huesped (claveHuesped, apPatHuesped, apMatHuesped, calleHues, CPHues, paisHues, telHues) values ('n807', 'Pyotr', 'Dobbings', '184 Summerview Place', 45254, 'China', '6607700657');
-insert into Huesped (claveHuesped, apPatHuesped, apMatHuesped, calleHues, CPHues, paisHues, telHues) values ('k546', 'Ashli', 'Cursey', '38864 Hollow Ridge Circle', 23462, 'Philippines', '4624357857');
-insert into Huesped (claveHuesped, apPatHuesped, apMatHuesped, calleHues, CPHues, paisHues, telHues) values ('j730', 'Saxe', 'Cowdrey', '74829 Sheridan Park', 68316, 'Vietnam', '3491744551');
-insert into Huesped (claveHuesped, apPatHuesped, apMatHuesped, calleHues, CPHues, paisHues, telHues) values ('q063', 'Flossi', 'Murcott', '71521 Talmadge Plaza', 10993, 'Poland', '8831168124');
-insert into Huesped (claveHuesped, apPatHuesped, apMatHuesped, calleHues, CPHues, paisHues, telHues) values ('k567', 'Ella', 'Dumbreck', '9066 Hoffman Terrace', 67794, 'France', '8747648473');
-insert into Huesped (claveHuesped, apPatHuesped, apMatHuesped, calleHues, CPHues, paisHues, telHues) values ('w443', 'Hillary', 'Tease', '426 Elka Plaza', 81544, 'China', '9086605163');
-insert into Huesped (claveHuesped, apPatHuesped, apMatHuesped, calleHues, CPHues, paisHues, telHues) values ('u695', 'Brodie', 'Strasse', '99 Mallory Place', 1280, 'China', '1485252912');
-insert into Huesped (claveHuesped, apPatHuesped, apMatHuesped, calleHues, CPHues, paisHues, telHues) values ('d319', 'Barr', 'Chattoe', '06 Esch Plaza', 60887, 'China', '8047237304');
-insert into Huesped (claveHuesped, apPatHuesped, apMatHuesped, calleHues, CPHues, paisHues, telHues) values ('u816', 'Riannon', 'Morrish', '872 West Trail', 60348, 'Argentina', '9865918104');
-insert into Huesped (claveHuesped, apPatHuesped, apMatHuesped, calleHues, CPHues, paisHues, telHues) values ('z879', 'Arlin', 'Chubb', '722 Monterey Parkway', 97862, 'Venezuela', '1378993018');
-insert into Huesped (claveHuesped, apPatHuesped, apMatHuesped, calleHues, CPHues, paisHues, telHues) values ('g558', 'Suzi', 'Hansen', '82 Farragut Point', 98486, 'Indonesia', '6864808336');
-insert into Huesped (claveHuesped, apPatHuesped, apMatHuesped, calleHues, CPHues, paisHues, telHues) values ('e256', 'Keeley', 'Dudden', '6936 Valley Edge Alley', 90254, 'Sweden', '8311302100');
-insert into Huesped (claveHuesped, apPatHuesped, apMatHuesped, calleHues, CPHues, paisHues, telHues) values ('z839', 'Fannie', 'McFeat', '7 Messerschmidt Junction', 947, 'China', '7017949899');
-insert into Huesped (claveHuesped, apPatHuesped, apMatHuesped, calleHues, CPHues, paisHues, telHues) values ('h397', 'Abraham', 'Pipet', '366 Saint Paul Circle', 72130, 'France', '8536026893');
-insert into Huesped (claveHuesped, apPatHuesped, apMatHuesped, calleHues, CPHues, paisHues, telHues) values ('v128', 'Ursala', 'Ortes', '42 Harbort Way', 46693, 'United States', '6012190104');
-insert into Huesped (claveHuesped, apPatHuesped, apMatHuesped, calleHues, CPHues, paisHues, telHues) values ('t476', 'Lu', 'Machon', '37124 Declaration Drive', 53719, 'Slovenia', '9273708875');
-insert into Huesped (claveHuesped, apPatHuesped, apMatHuesped, calleHues, CPHues, paisHues, telHues) values ('s875', 'Maxie', 'Jakoub', '73 Swallow Parkway', 3178, 'South Africa', '8392951701');
-insert into Huesped (claveHuesped, apPatHuesped, apMatHuesped, calleHues, CPHues, paisHues, telHues) values ('e909', 'Fonsie', 'Leythley', '8572 Killdeer Pass', 98727, 'China', '8417885767');
-insert into Huesped (claveHuesped, apPatHuesped, apMatHuesped, calleHues, CPHues, paisHues, telHues) values ('x551', 'Oneida', 'Glentworth', '78 Homewood Alley', 51001, 'China', '9277560167');
 
 -- Tabla tipoHabitacion
 
--- PLSQL.sql
+++ PLSQL.sql
@@ -4,7 +4,7 @@ Equipo:
 - Aldo Yael NAVARRETE ZAMORA
 - Daniel Eduardo JARQUIN LÓPEZ
 - Diego Ignacio NÚÑEZ HERNÁNDEZ
-- Uriel Quetzal TAVERA MARRTÍNEZ
+- Uriel Quetzal TAVERA MARTÍNEZ
 */
 
 /*
@@ -64,9 +64,7 @@ AS
 	FROM alojamiento
 	NATURAL JOIN alojaAct
 	NATURAL JOIN actividad
-	ORDER BY clvAlo
-;
-
+	ORDER BY clvAlo;
 
 /*
     Calcular las vacaciones del personal para un año determinado 
@@ -113,6 +111,7 @@ END LOOP;
 END spVacaciones;
 /
 
+exec spVacaciones;
 
 /*
     Guardar en una tabla llamada reporteReservaciones, las reservaciones 
@@ -129,34 +128,13 @@ CREATE TABLE reporteReservaciones(
 	inicioReservaReporte DATE
 );
 
-CREATE OR REPLACE TRIGGER tgRegistraReserva
-AFTER
-	INSERT 
-	ON reserva
-	FOR EACH ROW
-DECLARE
-	vnomHues VARCHAR2(30);
-	vnomAlo VARCHAR(30);
-BEGIN
-	SELECT nomHues, nomAlo
-	INTO vnomHues, vnomAlo
-	FROM reserva
-	NATURAL JOIN huesped
-	NATURAL JOIN alojamiento;
-
-	INSERT INTO reporteReservaciones
-	VALUES (:NEW.noHab, :NEW.clvAlo, :NEW.claveHuesped, vnomHues, 
-	vnomAlo, :NEW.inicioReserva);
-END tgRegistraReserva;
-/
-
 /*
     En el momento en que una reservación es cancelada el estatus de la 
     habitación debe de regresar a su estado original.
 */
 CREATE OR REPLACE VIEW vwReserva
 AS
-SELECT noHab, clvAlo, nomHues, inicioReserva, finReserva, estatusHab FROM RESERVA
+SELECT noHab, clvAlo, claveHuesped, nomHues, inicioReserva, finReserva FROM RESERVA
 NATURAL JOIN Huesped
 NATURAL JOIN HABITACION;
 
@@ -173,8 +151,6 @@ END tgReservacionCancelada;
 
 -- ************** PROCEDIMIENTOS ***************
 
-
-
 /*  
     PROCEDIMIENTO para crear alojamientos
 */
@@ -184,16 +160,16 @@ START WITH 1
 INCREMENT BY 1;
 
 CREATE OR REPLACE PROCEDURE spAltaAlojamiento(
-   nomAlo IN VARCHAR,
-   calleAlo IN VARCHAR,
-   CPAlo IN NUMBER,
-   paisAlo IN VARCHAR,
-   cantidadHab IN NUMBER,
-   telAlo IN NUMBER
+    nomAlo IN VARCHAR,
+    calleAlo IN VARCHAR,
+    CPAlo IN NUMBER,
+    paisAlo IN VARCHAR,
+    cantidadHab IN NUMBER,
+    telAlo IN NUMBER
 )
 AS
 BEGIN
-   INSERT INTO Alojamiento(clvAlo,nomAlo,calleAlo,CPAlo,paisAlo,cantidadHab,telAlo) VALUES (ID_ALO_SEQ.NEXTVAL,nomAlo,calleAlo,CPAlo,paisAlo,cantidadHab,telAlo);
+    INSERT INTO Alojamiento(clvAlo,nomAlo,calleAlo,CPAlo,paisAlo,cantidadHab,telAlo) VALUES (ID_ALO_SEQ.NEXTVAL,nomAlo,calleAlo,CPAlo,paisAlo,cantidadHab,telAlo);
 END spAltaAlojamiento;
 /
 
@@ -211,30 +187,30 @@ START WITH 1
 INCREMENT BY 1;
 
 CREATE OR REPLACE PROCEDURE spAltaPersonal(
-   nomPer IN VARCHAR,
-   apPatPer IN VARCHAR,
-   apMatPer IN VARCHAR,
-   callePer IN VARCHAR,
-   CPPer IN NUMBER,
-   paisPer IN VARCHAR,
-   fechaIngreso IN DATE,
-   RFCPer IN CHAR
+    nomPer IN VARCHAR,
+    apPatPer IN VARCHAR,
+    apMatPer IN VARCHAR,
+    callePer IN VARCHAR,
+    CPPer IN NUMBER,
+    paisPer IN VARCHAR,
+    fechaIngreso IN DATE,
+    RFCPer IN CHAR
 )
 AS
 BEGIN
-   INSERT INTO Personal(clavePersonal,nomPer,apPatPer,apMatPer,callePer,CPPer,paisPer,fechaIngreso,RFCPer) VALUES (ID_PER_SEQ.NEXTVAL,nomPer,apPatPer,apMatPer,callePer,CPPer,paisPer,fechaIngreso,RFCPer);
+    INSERT INTO Personal(clavePersonal,nomPer,apPatPer,apMatPer,callePer,CPPer,paisPer,fechaIngreso,RFCPer) VALUES (ID_PER_SEQ.NEXTVAL,nomPer,apPatPer,apMatPer,callePer,CPPer,paisPer,fechaIngreso,RFCPer);
 END spAltaPersonal;
 /
 
-EXEC spAltaPersonal('Vicente','Sierra','Cruz','Av.Chapultepec',85453,'Mexico',SYSDATE-6,'SICV840520QN4');
-EXEC spAltaPersonal('Daniel','Jarquin','Lopez','2 de Marzo',56334,'Mexico',SYSDATE-12,'JALD011030SG3');
-EXEC spAltaPersonal('Aldo','Navarrete','Zamora','Genaro Nuñez 18',07120,'Mexico',SYSDATE-8,'NAZA200301HD');
-EXEC spAltaPersonal('Diego','Nuñez','Hernandez','Juarez 19',21312,'Mexico',SYSDATE-9,'NHDI123124HS');
-EXEC spAltaPersonal('Quetzal','Tavera','Martinez','Degollado 215',41236,'Mexico',SYSDATE-4,'TAMQ010605H82');
-EXEC spAltaPersonal('Ana','Fragoso','Islas','Constitucion 17',54879,'Mexico',SYSDATE-34,'FAIA001218EM4');
-EXEC spAltaPersonal('Edyth','Dunsford',NULL,'1416 Hamilton Street',12312,'Canada',SYSDATE-3,'DUE001218Q710');
-EXEC spAltaPersonal('Merle','Sauriol',NULL,'42 Place du Jeu de Paume',54321,'Francia',SYSDATE-4,'SQME13412HDS');
-EXEC spAltaPersonal('Freddie','Law',NULL,'213 Harvest Lane',12341,'Estados Unidos',SYSDATE-1,'LFW23GF234ASD');
+EXEC spAltaPersonal('Vicente','Sierra','Cruz','Av.Chapultepec',85453,'Mexico','13/04/2010','SICV840520QN4');
+EXEC spAltaPersonal('Daniel','Jarquin','Lopez','2 de Marzo',56334,'Mexico','23/12/2005','JALD011030SG3');
+EXEC spAltaPersonal('Aldo','Navarrete','Zamora','Genaro Nuñez 18',07120,'Mexico','7/06/2022','NAZA200301HD');
+EXEC spAltaPersonal('Diego','Nuñez','Hernandez','Juarez 19',21312,'Mexico','9/07/2015','NHDI123124HS');
+EXEC spAltaPersonal('Quetzal','Tavera','Martinez','Degollado 215',41236,'Mexico','12/12/2010','TAMQ010605H82');
+EXEC spAltaPersonal('Ana','Fragoso','Islas','Constitucion 17',54879,'Mexico','17/06/1986','FAIA001218EM4');
+EXEC spAltaPersonal('Edyth','Dunsford',NULL,'1416 Hamilton Street',12312,'Canada','18/09/2003','DUE001218Q710');
+EXEC spAltaPersonal('Merle','Sauriol',NULL,'42 Place du Jeu de Paume',54321,'Francia','02/02/2001','SQME13412HDS');
+EXEC spAltaPersonal('Freddie','Law',NULL,'213 Harvest Lane',12341,'Estados Unidos','01/01/1999','LFW23GF234ASD');
 
 /* 
     PROCEDIMIENTO Para Asignar un Personal a Alojamiento 
@@ -255,24 +231,243 @@ BEGIN
     WHERE clavePersonal = vclvPersonal;
 END;
 /
+--Diego, Sur
+EXEC spAsignaPersonalAlo('13','1'); 
+--Quetzal, Oriente
+EXEC spAsignaPersonalAlo('14','3'); 
+--Dani, Norte
+EXEC spAsignaPersonalAlo('11','2'); 
 
+/*  
+    PROCEDIMIENTO para Dar de Alta a Huesped
+*/
 
-EXEC spAsignaPersonalAlo('9','6'); --Diego, Sur
-EXEC spAsignaPersonalAlo('10','8');--Quetzal, Oriente
-EXEC spAsignaPersonalAlo('7','7'); --Dani, Norte
+CREATE SEQUENCE ID_HUES_SEQ
+START WITH 1
+INCREMENT BY 1;
 
 CREATE OR REPLACE PROCEDURE spAltaHuesped(
-   nomHues IN VARCHAR,
-   apPatHues IN VARCHAR,
-   apMatHues IN VARCHAR,
-   calleHues IN VARCHAR,
-   CPHues IN NUMBER,
-   paisHues IN VARCHAR,
-   fechaIngreso IN DATE,
-   RFCPer IN CHAR
+    nomHues IN VARCHAR,
+    apPatHues IN VARCHAR,
+    apMatHues IN VARCHAR,
+    calleHues IN VARCHAR,
+    CPHues IN NUMBER,
+    paisHues IN VARCHAR,
+    telHues IN NUMBER
 )
 AS
 BEGIN
-   INSERT INTO Personal(clavePersonal,nomPer,apPatPer,apMatPer,callePer,CPPer,paisPer,fechaIngreso,RFCPer) VALUES (ID_PER_SEQ.NEXTVAL,nomPer,apPatPer,apMatPer,callePer,CPPer,paisPer,fechaIngreso,RFCPer);
-END spAltaPersonal;
+    INSERT INTO Huesped(claveHuesped,nomHues,apPatHuesped,apMatHuesped,calleHues,CPHues,paisHues,telHues) VALUES (ID_HUES_SEQ.NEXTVAL,nomHues,apPatHues,apMatHues,calleHues,CPHues,paisHues,telHues);
+END spAltaHuesped;
 /
+
+EXEC spAltaHuesped('Hugo','Revilla','Gomez','Villaurrutia',54112,'Mexico',5575865145);
+EXEC spAltaHuesped('Juana','Breacher','Hansen','68 Hagan Point',84959,'Brazil',2479321805);
+EXEC spAltaHuesped('Riannon','Morrish','Murcott','872 West Trail',60348,'Argentina',9865918104);
+EXEC spAltaHuesped('Ellswerth','Dacca','Fudger','3 Petterle Point',78518,'Venezuela',2318026033);
+EXEC spAltaHuesped('Lilia','Dani','Sorrell','48 Starling Trail',38955,'Paraguay',3442187386);
+EXEC spAltaHuesped('Ursala','Ortes','Dacca','42 Harbort Way',46693,'United States',6012190104);
+
+/*  
+    PROCEDIMIENTO para Crear un nuevo TIPO de Habitacion
+*/
+
+CREATE OR REPLACE PROCEDURE spAltaTipoHabitacion(
+    tipoHab IN VARCHAR,
+    precioHab IN NUMBER
+)
+AS
+BEGIN 
+    INSERT INTO tipoHabitacion VALUES (tipoHab,precioHab);
+END spAltaTipoHabitacion;
+/
+
+EXEC spAltaTipoHabitacion('INDIVIDUAL',2400);
+EXEC spAltaTipoHabitacion('DOBLE',4800);
+EXEC spAltaTipoHabitacion('TRIPLE',7200);
+
+/*  
+    PROCEDIMIENTO dar de Alta Habitación
+*/
+
+CREATE OR REPLACE PROCEDURE spAltaHabitacion(
+    noHab IN CHAR,
+    clvAlo IN CHAR,
+    tipoHab IN VARCHAR,
+    estatusHab IN VARCHAR
+)
+AS 
+BEGIN 
+    INSERT INTO habitacion VALUES (noHab,clvAlo,tipoHab,estatusHab);
+END;
+/
+
+-- Habitaciones Alojamiento Norte 
+EXEC spALtaHabitacion('1','2','INDIVIDUAL','DISPONIBLE');
+EXEC spALtaHabitacion('2','2','DOBLE','DISPONIBLE');
+EXEC spALtaHabitacion('3','2','TRIPLE','DISPONIBLE');
+
+-- Habitaciones Alojamiento Sur
+EXEC spALtaHabitacion('1','1','INDIVIDUAL','DISPONIBLE');
+EXEC spALtaHabitacion('2','1','DOBLE','DISPONIBLE');
+EXEC spALtaHabitacion('3','1','TRIPLE','DISPONIBLE');
+
+-- Habitaciones Alojamiento Oriente 
+EXEC spALtaHabitacion('1','3','INDIVIDUAL','DISPONIBLE');
+EXEC spALtaHabitacion('2','3','DOBLE','DISPONIBLE');
+EXEC spALtaHabitacion('3','3','TRIPLE','DISPONIBLE');
+
+/*  
+    PROCEDIMIENTO para Crear una actividad
+*/
+
+CREATE SEQUENCE ID_ACT_SEQ
+START WITH 1
+INCREMENT BY 1;
+
+CREATE OR REPLACE PROCEDURE spAltaActividad(
+    vNomAct IN VARCHAR,
+    vdescAct IN VARCHAR,
+    vnivelAct IN VARCHAR
+)
+AS
+BEGIN
+    INSERT INTO Actividad(clvAct,nomAct,descAct,nivelAct) VALUES(ID_ACT_SEQ.NEXTVAL,vnomAct,vdescAct,vnivelAct);
+END;
+/
+
+EXEC spAltaActividad('Senderismo','Se realizará una caminata por el area natural mas cercana al alojamiento. Duracion promedio de 3 horas.','MEDIO');
+EXEC spAltaActividad('Bicicleta de montania','Los ciclistas podran usar las bicicletas de uso rudo del alojamiento, y realizar recorridos por rutas complejas y empinadas.','ALTO');
+EXEC spAltaActividad('Cabalgata','Los participantes montaran un caballo del establo con ayuda del personal del alojamiento, y daran un paseo por los alrededores.','BAJO');
+EXEC spAltaActividad('Viaje en tranvia','Recorrido de los alrrededores de la ciudad, a bordo de un tranvia tematico.','BAJO');
+EXEC spAltaActividad('Lanzamiento de tirolesa','El participante se lanzara desde la tirolesa, y se desplazara a alta velocidad mientras posee una vista completa de la ciudad','ALTO');
+
+/*  
+    PROCEDIMIENTO dar de Alta una Actividad en un Alojamiento
+*/
+
+CREATE OR REPLACE PROCEDURE spAltaAlojaAct(
+    vClvAlo IN CHAR,
+    vClvAct IN VARCHAR,
+    vFechaActividad IN VARCHAR,
+    vHorario IN VARCHAR
+)
+AS
+BEGIN
+    INSERT INTO alojaAct 
+    VALUES(vClvAlo,vClvAct,vFechaActividad,vHorario);
+END;
+/
+
+EXEC spAltaAlojaAct(1,3,'Martes','11am-6pm');
+EXEC spAltaAlojaAct(1,1,'Jueves','8am-2pm');
+EXEC spAltaAlojaAct(2,2,'Miercoles','1pm-7pm');
+EXEC spAltaAlojaAct(2,5,'Jueves','10am-6pm');
+EXEC spAltaAlojaAct(3,4,'Viernes','6pm-9pm');
+EXEC spAltaAlojaAct(3,3,'Miercoles','6pm-9pm');
+
+/*
+    PROCEDIMIENTO para Hacer una Reserva 
+*/
+SET SERVEROUTPUT ON;
+
+/* 
+    DISPARADOR de Sustitución cuando se hace una reserva se cambia el status
+    en la habitación y la agrega a la tabla reserva
+*/
+CREATE OR REPLACE TRIGGER tgReservacionCreada
+INSTEAD OF INSERT ON vwReserva --Opera en la vista
+FOR EACH ROW
+DECLARE 
+    vNomHuesped VARCHAR2(30);
+    vNomAlo VARCHAR(30);
+BEGIN
+    SELECT NOMHUES
+    INTO vNomHuesped 
+    FROM HUESPED
+    WHERE claveHuesped = :NEW.claveHuesped;
+
+    SELECT nomAlo
+    INTO vNomAlo
+    FROM  Alojamiento
+    WHERE clvAlo = :NEW.clvAlo; 
+
+    INSERT INTO RESERVA VALUES (:NEW.noHab,:NEW.clvAlo,:NEW.claveHuesped,:NEW.inicioReserva,:NEW.finReserva);
+    INSERT INTO reporteReservaciones VALUES (:NEW.noHab, :NEW.clvAlo, :NEW.claveHuesped, vNomHuesped, vnomAlo, :NEW.inicioReserva);
+
+    --Actualiza el estado habitacion
+    UPDATE HABITACION SET estatusHab = 'OCUPADO'
+	WHERE noHab = :NEW.noHab
+    AND clvAlo = :NEW.clvAlo;
+
+END tgReservacionCreada;
+/
+
+CREATE OR REPLACE PROCEDURE spCreaReserva(
+    vnoHab in CHAR,
+    vclvAlo in CHAR,
+    vclaveHuesped in CHAR,
+    vinicioReserva in DATE,
+    vfinReserva in DATE
+)
+AS
+    vNomHuesped VARCHAR2(30);
+    vNomAlo VARCHAR2(30);
+    vEstatusHab2 VARCHAR2(15);
+BEGIN
+    SELECT estatusHab
+    INTO vEstatusHab2
+    FROM habitacion
+    WHERE noHab = vnoHab
+    AND clvAlo = vClvAlo;
+
+    IF vEstatusHab2 = 'DISPONIBLE' THEN
+        SELECT NOMHUES
+        INTO vNomHuesped
+        FROM HUESPED
+        WHERE claveHuesped = vclaveHuesped;
+
+        SELECT nomAlo
+        INTO vNomAlo
+        FROM  Alojamiento
+        WHERE clvAlo = vclvAlo; 
+        
+        INSERT INTO vwReserva VALUES (vnoHab,vclvAlo,vclaveHuesped,vNomHuesped,vinicioReserva,vfinReserva);
+        DBMS_OUTPUT.PUT_LINE(vNomHuesped || ' ha reservado exitosamente de '||vinicioReserva||' - '||vfinReserva|| ' en el alojamiento '|| vNomAlo);
+    ELSE
+        DBMS_OUTPUT.PUT_LINE('La habitacion seleccionada ya esta ocupada o no existe');
+    END IF;
+END spCreaReserva;
+/
+
+exec spCreaReserva('1','2','1',SYSDATE,SYSDATE+7);
+
+/*
+    PROCEDIMIENTO para Cancelar una Reserva 
+*/
+
+CREATE OR REPLACE PROCEDURE spCancelaReserva( 
+    vNoHab IN CHAR,
+    vClvAlo IN CHAR,
+    vClaveHuesped IN CHAR
+)
+AS
+BEGIN
+    DELETE FROM vwReserva
+    WHERE noHab = vNoHab
+    AND clvAlo = vClvAlo
+    AND ClaveHuesped = vClaveHuesped; 
+END;
+/
+
+exec spCancelaReserva('1','2','1');
+
+--- ************** TRIGGERS *****************
+
+/* Comando para checar los triggers creados por el usuario */
+SELECT
+    trigger_name 
+FROM 
+    user_triggers
+ORDER BY
+    trigger_name ASC;
